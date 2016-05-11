4bc2ac65c7f7a7ea663fa96a394e778490a0ffb8
Aushomapto Prodip <prodip_sarker@diu.edu.bd>
2016-05-12 02:00:09 +0600

    code

---
+++ UserProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management.Model
+{
+    public class UserProfile
+    {
+        public string RollNo { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string NickName { get; set; }        
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public string PostOffice { get; set; }
+        public string District { get; set; }
+        public string Country { get; set; }
+
+    }
+}
